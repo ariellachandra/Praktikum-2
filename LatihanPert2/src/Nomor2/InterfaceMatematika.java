7a392b649054d19d46b5ef4047c9b3b0aeccf6cd
ariellachandra <164894955+ariellachandra@users.noreply.github.com>
2024-09-09 01:05:58 +0700

    Add files via upload

---
+++ LatihanPert2/src/Nomor2/InterfaceMatematika.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Nomor2;
+
+/**
+ *
+ * @author lenovo
+ */
+interface InterfaceMatematika {
+    void pertambahan (int a, int b);
+    void pengurangan (int a, int b);
+    void perkalian (int a, int b);
+    void pembagian (int a, int b);
+}
---
+++ LatihanPert2/src/Nomor3/Bingo.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Nomor3;
+
+/**
+ *
+ * @author lenovo
+ */
+public class Bingo {
+    String clap, clap1, clap2, clap3, clap4, clap5;
+    
+    public void tampil(){
+        System.out.println (this.clap);
+        System.out.println (" ");
+        System.out.println (this.clap1);
+        System.out.println (" ");
+        System.out.println (this.clap2);
+        System.out.println (" ");
+        System.out.println (this.clap3);
+        System.out.println (" ");
+        System.out.println (this.clap4);
+        System.out.println (" ");
+        System.out.println (this.clap5);
+    }
+}
